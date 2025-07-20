5a8cb70bde4f37b009056ccba9baeb5d2f3ca0ac
Sahitya33 <sahityabhujel1738@gmail.com>
2025-07-20 22:27:19 +0545

    Add files via upload

---
+++ BMI_calculator_streamlit.py
@@ -0,0 +1,22 @@
+import streamlit as st
+st.title("BMI Calculator")
+weight = st.number_input("Enter your bodyweight in kg:")
+height = st.number_input("Enter your height in feet:")
+
+BMI_button =st.button('BMI')
+
+
+if BMI_button:
+
+    BMI= weight / (((height/3.28))**2)
+
+    if BMI<16:
+        st.error("You're extremely underweight")
+    elif BMI>=16 and BMI<18.5:
+        st.warning("You're underweight")
+    elif BMI>=18.5 and BMI<25:
+        st.success("You're healthy")
+    elif BMI>=25 and BMI<30:
+        st.info("You're overweight")
+    elif BMI>=30:
+        st.error("You're extrmely overweight")
