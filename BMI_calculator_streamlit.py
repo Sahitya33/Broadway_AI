79773f75989bfb754b7719eb4646b42f3dd5a259
Sahitya33 <sahityabhujel1738@gmail.com>
2025-07-21 16:39:14 +0545

    Update BMI_calculator_streamlit.py

--- BMI_calculator_streamlit.py
+++ BMI_calculator_streamlit.py
@@ -9,14 +9,15 @@ BMI_button =st.button('BMI')
 if BMI_button:
 
     BMI= weight / (((height/3.28))**2)
+    BMI=round(BMI,2)
 
     if BMI<16:
-        st.error("You're extremely underweight")
+        st.error(f"Your BMI is {BMI}.You're extremely underweight")
     elif BMI>=16 and BMI<18.5:
-        st.warning("You're underweight")
+        st.warning(f"Your BMI is {BMI}.You're underweight")
     elif BMI>=18.5 and BMI<25:
-        st.success("You're healthy")
+        st.success(f"Your BMI is {BMI}.You're healthy")
     elif BMI>=25 and BMI<30:
-        st.info("You're overweight")
+        st.info(f"Your BMI is {BMI}.You're overweight")
     elif BMI>=30:
-        st.error("You're extrmely overweight")
+        st.error(f"Your BMI is {BMI}.You're extrmely overweight")
